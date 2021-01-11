52ab1e99b5e64bd1644cc944ef4228360c52d81c
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2021-01-11 11:47:01 -0500

    http-data completed

--- src/app/services/http-data.service.ts
+++ src/app/services/http-data.service.ts
@@ -1,9 +1,53 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {Graduate} from '../models/graduate';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpDataService {
+  basePath = 'http://localhost:8080/api/graduate';
+  constructor(private http: HttpClient) { }
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type' : 'application/json'
+    })
+  };
 
-  constructor() { }
+  handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      console.log('An error occurred: ', error.error.message);
+    }
+    else {
+      console.log(`Backend returned code ${error.status}, body was: ${error.error}`);
+    }
+    return throwError('Something happened with request, please try again later.');
+  }
+
+  createItem(item): Observable<Graduate> {
+    return this.http.post<Graduate>(this.basePath, JSON.stringify(item), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  getItem(id): Observable<Graduate> {
+    return this.http.get<Graduate>(`${this.basePath}/${id}`, this.httpOptions )
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  getList(): Observable<Graduate>{
+    return this.http.get<Graduate>(this.basePath)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  updateItem(id, item): Observable<Graduate>{
+    return this.http.put<Graduate>(`${this.basePath}/${id}`, JSON.stringify(item), this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
+
+  deleteItem(id): Observable<any> {
+    return this.http.delete<Graduate>(`${this.basePath}/${id}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
+  }
 }
