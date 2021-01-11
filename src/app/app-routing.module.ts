b00d87013893d2e423e2aaf541e50f01b5126f39
Aaron Alberto Alva Caffo <34250041+CaffoAaron@users.noreply.github.com>
2021-01-11 12:20:54 -0500

    component completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GraduatesComponent} from './pages/graduates/graduates.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: GraduatesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
